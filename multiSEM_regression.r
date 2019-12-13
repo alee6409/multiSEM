da1606efeb3ec6e9686786f51f02ec52ec0716b8
zhiyi lee <zhiyilee@zhiyis-MacBook-Pro.local>
2019-12-13 08:51:32 +0800

    regression

---
+++ multiSEM_regression.r
@@ -0,0 +1,24 @@
+library(lavaan)
+library(lavaan.survey)
+
+model <- readLines('tj2019_2.lav')
+
+fit.naive <- sem(model, data = data1, std.lv=TRUE, estimator="MLM")
+survey.design <- svydesign(ids = ~schids, prod = ~1, data = data1)
+
+fit.survey <- lavaan.survey(fit.naive, survey.design)
+summary(fit.survey)
+
+library(semPlot)
+semPaths(fit.survey,what = 'paths', 
+         style = 'OpenMX', layout = 'tree', 
+         edge.label.cex = 1.25, nCharNodes = 0, 
+         sizeMan = 8, sizeLat = 12, 
+         residuals = FALSE, intercepts = FALSE)
+
+#描述统计
+library(psych)
+describeBy(data1, data1$schids, ranges=FALSE, skew=FALSE)
+
+fit_aov <- aov(bad~schids, data = data1)
+summary(fit_aov)
